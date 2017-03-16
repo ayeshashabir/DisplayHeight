3432eb7df2c57dd85028ffb4fd07792cf46ff0ca
Alsaba <Alsaba@DESKTOP-FOLTJLP>
2017-03-16 12:02:05 -0700

    programming in java

---
+++ src/display/height/DisplayHeight.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package display.height;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alsaba
+ */
+public class DisplayHeight {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter your height");
+        float h;
+        h =scan.nextFloat();
+        System.out.println("this is my height:"+h);
+        
+       
+        // TODO code application logic here
+    }
+    
+}
